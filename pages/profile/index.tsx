b69ae26c1db505fadefb9830a95f9c50b0c69433
nova_44056 <senguy44056@gmail.com>
2024-08-19 20:31:05 -0500

    feat: Refactor

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -115,7 +115,7 @@ export default function ProfilePage() {
               lightColor="#0000"
             />
             <div className="text-center text-[#170F49] text-md font-semibold">
-              {profile?.user.group ? profile.user.group : 'Group TBD'}
+              {profile?.user.group ? profile?.user.group : 'Group TBD'}
             </div>
           </div>
           <div className="border-y-[1.2px] border-primaryDark/20 py-4 md:my-8 my-6">
@@ -133,13 +133,13 @@ export default function ProfilePage() {
                   : 'text-[#5C67C9]'
               }`}
             >
-              {profile?.status ? profile.status : 'In Review'}
+              {profile?.status ? profile?.status : 'In Review'}
             </h1>
           </div>
 
           <div className="flex gap-x-4">
             <div className="flex items-center justify-center w-10 h-10 rounded-full">
-              <a href={profile.linkedin} target="_blank" rel="noreferrer">
+              <a href={profile?.linkedin} target="_blank" rel="noreferrer">
                 <Image
                   alt="LinkedIn"
                   src={LinkedInImage.src}
@@ -149,12 +149,12 @@ export default function ProfilePage() {
               </a>
             </div>
             <div className="flex items-center justify-center w-10 h-10 rounded-full">
-              <a href={profile.github} target="_blank" rel="noreferrer">
+              <a href={profile?.github} target="_blank" rel="noreferrer">
                 <GitHubIcon className="!w-10 !h-10" />
               </a>
             </div>
             <div className="flex items-center justify-center w-10 h-10 rounded-full">
-              <a href={profile.website} target="_blank" rel="noreferrer">
+              <a href={profile?.website} target="_blank" rel="noreferrer">
                 <LanguageRoundedIcon className="text-[#5C67C9] !w-10 !h-10" />
               </a>
             </div>
@@ -187,49 +187,49 @@ export default function ProfilePage() {
 
         {/* Info */}
         <div className="w-full">
-          <h1 className="text-center font-fredoka font-semibold text-5xl md:mt-0 mt-10 text-[#40B7BA]">{`${profile.user.firstName} ${profile.user.lastName}`}</h1>
+          <h1 className="text-center font-fredoka font-semibold text-5xl md:mt-0 mt-10 text-[#40B7BA]">{`${profile?.user.firstName} ${profile?.user.lastName}`}</h1>
 
           <div className="w-full grid gap-8 grid-cols-1 md:grid-cols-2 mt-8">
             <TextField
               className="col-span-1"
               disabled
               label="University"
-              value={profile.university}
+              value={profile?.university}
               {...textFieldOverrides}
             />
             <TextField
               className="col-span-1"
               disabled
               label="Major"
-              value={profile.major}
+              value={profile?.major}
               {...textFieldOverrides}
             />
             <TextField
               className="col-span-1"
               disabled
               label="Role"
-              value={formatRole(profile.user.permissions[0])}
+              value={formatRole(profile?.user.permissions[0])}
               {...textFieldOverrides}
             />
             <TextField
               className="col-span-1"
               disabled
               label="Number of hackathons attended"
-              value={profile.hackathonExperience}
+              value={profile?.hackathonExperience}
               {...textFieldOverrides}
             />
             <TextField
               className="col-span-2"
               disabled
               label="Current level of study"
-              value={formatStudyLevel(profile.studyLevel)}
+              value={formatStudyLevel(profile?.studyLevel)}
               {...textFieldOverrides}
             />
             <TextField
               className="col-span-2"
               disabled
               label="Preferred email"
-              value={profile.user.preferredEmail}
+              value={profile?.user.preferredEmail}
               {...textFieldOverrides}
             />
           </div>
