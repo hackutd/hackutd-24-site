f0b9f7860ba581ab71c1818e0b4a668bb7787399
Rayyan Waris <raywa04@gmail.com>
2024-09-12 12:55:52 -0500

    feat: Add smooth transition scroll for faq

--- components/homeComponents/FaqDisclosure.tsx
+++ components/homeComponents/FaqDisclosure.tsx
@@ -1,6 +1,7 @@
-import { Disclosure, Transition } from '@headlessui/react';
+import { Disclosure } from '@headlessui/react';
 import { PlusIcon, MinusIcon } from '@heroicons/react/solid';
 import Markdown from 'react-markdown';
+import { useState, useRef, useEffect } from 'react';
 
 /**
  *
@@ -30,6 +31,17 @@ export default function FaqDisclosure({
   isOpen,
   toggleDisclosure,
 }: FaqDisclosureProps) {
+  const [maxHeight, setMaxHeight] = useState('0px');
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      setMaxHeight(`${contentRef.current?.scrollHeight}px`);
+    } else {
+      setMaxHeight('0px');
+    }
+  }, [isOpen]);
+
   return (
     <Disclosure>
       <div
@@ -38,42 +50,39 @@ export default function FaqDisclosure({
         }}
         className="transition duration-500 ease-in-out bg-white rounded-md p-4 mx-5"
       >
-        <Disclosure.Button className={`p-2 text-[#6F6C90] font-medium text-left  w-full`} as="div">
+        <Disclosure.Button as="div">
           <button
-            className="w-full flex flex-row justify-between items-center"
-            onClick={() => {
-              toggleDisclosure();
-            }}
+            className="w-full flex justify-between items-center p-2 text-[#6F6C90] font-medium"
+            onClick={toggleDisclosure}
           >
             <h1 style={{ fontFamily: 'Fredoka', color: '#170F49' }} className="text-left text-xl">
               {question}
             </h1>
+
             <div
               style={{ backgroundColor: !isOpen ? '#F7F7FB' : '#54DDE8' }}
               className="p-3 rounded-md transition duration-500 ease-in-out"
             >
               {!isOpen ? (
-                <PlusIcon className={'transition duration-500 ease-in-out w-5 h-5'} />
+                <PlusIcon className="transition transform duration-300 ease-in-out w-5 h-5" />
               ) : (
-                <MinusIcon className={`transition duration-500 ease-in-out w-5 h-5 text-white`} />
+                <MinusIcon className="transition transform duration-300 ease-in-out w-5 h-5 text-white" />
               )}
             </div>
           </button>
         </Disclosure.Button>
 
-        {/* {isOpen && ( */}
-        <Transition
-          show={isOpen}
-          enter="transition duration-100 ease-out"
-          enterFrom="transform scale-95 opacity-0"
-          enterTo="transform scale-100 opacity-100"
-          leave="transition duration-75 ease-out"
-          leaveFrom="transform scale-100 opacity-100"
-          leaveTo="transform scale-95 opacity-0"
+        <div
+          ref={contentRef}
+          style={{
+            maxHeight: maxHeight,
+            transition: 'max-height 0.5s ease',
+            overflow: 'hidden',
+          }}
         >
           <Disclosure.Panel
             style={{ color: '#6F6C90' }}
-            className={`my-2 py-2  p-2 text-left text-sm`}
+            className="my-2 py-2 p-2 text-left text-sm"
             static
           >
             <Markdown
@@ -87,8 +96,7 @@ export default function FaqDisclosure({
               {answer}
             </Markdown>
           </Disclosure.Panel>
-        </Transition>
-        {/* )} */}
+        </div>
       </div>
     </Disclosure>
   );
