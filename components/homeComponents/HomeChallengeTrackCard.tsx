08cfe147ea5a1ab27d90366ebf8fbc019221819f
Rayyan Waris <raywa04@gmail.com>
2024-09-19 17:37:15 -0500

    feat: Add wobble card effect

--- components/homeComponents/HomeChallengeTrackCard.tsx
+++ components/homeComponents/HomeChallengeTrackCard.tsx
@@ -1,11 +1,39 @@
+'use client';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
+import React, { useState } from 'react';
 
 export default function HomeChallengeTrackCard(props: {
   challengeTrack: { title: string; subtitle: string; description: string; imgSrc: string };
   blockType: number;
 }) {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLElement>) => {
+    const { clientX, clientY } = event;
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = (clientX - (rect.left + rect.width / 2)) / 20;
+    const y = (clientY - (rect.top + rect.height / 2)) / 20;
+    setMousePosition({ x, y });
+  };
+
   return (
-    <div className="mx-auto bg-white rounded-lg w-[350px] m-8">
+    <motion.div
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => {
+        setIsHovering(false);
+        setMousePosition({ x: 0, y: 0 });
+      }}
+      style={{
+        transform: isHovering
+          ? `translate3d(${mousePosition.x}px, ${mousePosition.y}px, 0) scale3d(1, 1, 1)`
+          : 'translate3d(0px, 0px, 0) scale3d(1, 1, 1)',
+        transition: 'transform 0.1s ease-out',
+      }}
+      className="mx-auto bg-white rounded-lg w-[350px] m-8"
+    >
       <div
         className={`mx-auto bg-gradient-to-b from-[#F5A3B6] from-10% to-[#F7CE79] rounded-t-md flex justify-center relative h-[220px]`}
       >
@@ -29,6 +57,6 @@ export default function HomeChallengeTrackCard(props: {
           <p className="text-md line-clamp-5 text-balance">{props.challengeTrack.description}</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
