346438e6c41a75d96e45c65fce72c7d6f66321a7
Rayyan Waris <raywa04@gmail.com>
2024-09-19 12:23:10 -0500

    feat: Add wobble card effect

--- components/homeComponents/HomePrizes.tsx
+++ components/homeComponents/HomePrizes.tsx
@@ -9,26 +9,17 @@ interface HomePrizesProps {
 
 export default function HomePrizes({ prizes }: HomePrizesProps) {
   return (
-    <section id="prizes-section" className="w-full overflow-hidden">
-      {/* Banner */}
-      <div className="-mx-4 mt-[120px] -rotate-[7deg] bg-[#7B81FF] text-white py-[0.35rem] w-[2000px] overflow-hidden line-clamp-1">
-        <p>
-          {Array.apply(null, Array(200))
-            .map(() => 'PRIZES')
-            .join('  ')}
-        </p>
-      </div>
-      {/* Component */}
-      <div className="md:py-12 py-6 xl:w-9/10 w-11/12 m-auto">
-        <h1 className="font-fredoka mt-[100px] text-3xl text-center font-bold text-[#05149C]">
-          Prizes
-        </h1>
-        <p className="text-center">Potential prize that participants can win!</p>
-        <div className="md:grid md:grid-cols-3 flex flex-col gap-x-6 gap-y-[140px] mt-6">
-          {prizes.map((prize, idx) => (
-            <HomePrizesCard key={idx} prize={prize} blockType={idx % 3} />
-          ))}
-        </div>
+    <section id="prizes-section" className="w-full overflow-hidden py-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-[25px] gap-y-[25px] max-w-[1140px] mx-auto px-4 sm:px-8 lg:px-0">
+        {prizes.map((prize, idx) => (
+          <HomePrizesCard
+            key={idx}
+            prize={prize}
+            blockType={idx % 3}
+            sponsorTitle=""
+            challengeTitle=""
+          />
+        ))}
       </div>
     </section>
   );
