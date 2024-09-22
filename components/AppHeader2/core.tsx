d7a5872fb471a111c52c2cb6d34cd712a4f32ed2
doanhtu07 <doanhtu07@gmail.com>
2024-09-22 15:54:54 -0500

    feat: Add Admin to top row

--- components/AppHeader2/core.tsx
+++ components/AppHeader2/core.tsx
@@ -11,7 +11,6 @@ import { SectionReferenceContext } from '@/lib/context/section';
 import { NavbarCallbackRegistryContext } from '@/lib/context/navbar';
 import FloatingDockWrapper from './FloatingDock/wrapper';
 import clsx from 'clsx';
-import signOut from '@/pages/auth/signOut';
 
 type Props = {
   dockItemIdRoot?: string;
@@ -27,7 +26,7 @@ type Scan = {
 };
 
 export default function AppHeader2_Core(props: Props) {
-  const { user, hasProfile } = useAuthContext();
+  const { user, hasProfile, signOut } = useAuthContext();
   const { faqRef, scheduleRef } = useContext(SectionReferenceContext);
   const { callbackRegistry } = useContext(NavbarCallbackRegistryContext);
 
@@ -119,6 +118,88 @@ export default function AppHeader2_Core(props: Props) {
       itemIdx += 1;
     });
 
+    if (isAdmin) {
+      items.push(
+        <Menu id={itemIdRoot + itemIdx} as="div">
+          <Menu.Button
+            className={clsx(
+              'py-2 px-4 text-[#40B7BA] cursor-pointer flex gap-1 items-center justify-center font-bold',
+              'hover:bg-[#DFFEFF] transition-[background] duration-300 ease-in-out',
+              'rounded-[20px]',
+            )}
+          >
+            <div className="text-[#40B7BA]">Admin</div>
+            <svg
+              xmlns="http:www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#40B7BA"
+              className="size-4"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>
+          </Menu.Button>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 mt-2 w-full origin-top-right divide-x divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none flex">
+              {isSuperAdmin && (
+                <div className="px-1 py-1 w-1/4">
+                  <AdminNavbarColumn
+                    sectionTitle="Admin"
+                    options={[
+                      {
+                        optionName: 'User Dashboard',
+                        onClick: () => router.push('/admin/users'),
+                      },
+                      {
+                        optionName: 'Stats at a Glance',
+                        onClick: () => router.push('/admin/stats'),
+                      },
+                    ]}
+                  />
+                </div>
+              )}
+
+              <div className="w-1/2 px-1 py-1">
+                <AdminNavbarGrid
+                  numCols={2}
+                  sectionTitle="Temporary Scans"
+                  options={scanList
+                    .filter((scan) => !scan.isPermanentScan)
+                    .map((scan) => ({
+                      optionName: scan.name,
+                      onClick: () => setCurrentScan(scan),
+                    }))}
+                />
+              </div>
+
+              <div className="px-1 py-1">
+                <AdminNavbarColumn
+                  sectionTitle="Permanent Scans"
+                  options={scanList
+                    .filter((scan) => scan.isPermanentScan)
+                    .map((scan) => ({
+                      optionName: scan.name,
+                      onClick: () => setCurrentScan(scan),
+                    }))}
+                />
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>,
+      );
+      itemIdx++;
+    }
+
     // Profile/Apply button
     // TODO: Read after applications open
     items.push(
@@ -141,93 +222,6 @@ export default function AppHeader2_Core(props: Props) {
         )}
       </div>,
     );
-
-    return items;
-  };
-
-  const adminDockItems = () => {
-    const items: JSX.Element[] = [];
-    const itemIdRoot: string = (props.dockItemIdRoot ?? 'AppHeader2-Core-adminDockItems') + '_';
-    let itemIdx = 0;
-
-    items.push(
-      <Menu id={itemIdRoot + itemIdx} as="div">
-        <Menu.Button
-          className={clsx(
-            'py-2 px-4 text-[#40B7BA] cursor-pointer flex gap-1 items-center justify-center font-bold',
-            'hover:bg-[#DFFEFF] transition-[background] duration-300 ease-in-out',
-            'rounded-[20px]',
-          )}
-        >
-          <div className="text-[#40B7BA]">Admin</div>
-          <svg
-            xmlns="http:www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="#40B7BA"
-            className="size-4"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
-        </Menu.Button>
-
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 mt-2 w-full origin-top-right divide-x divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none flex">
-            {isSuperAdmin && (
-              <div className="px-1 py-1 w-1/4">
-                <AdminNavbarColumn
-                  sectionTitle="Admin"
-                  options={[
-                    {
-                      optionName: 'User Dashboard',
-                      onClick: () => router.push('/admin/users'),
-                    },
-                    {
-                      optionName: 'Stats at a Glance',
-                      onClick: () => router.push('/admin/stats'),
-                    },
-                  ]}
-                />
-              </div>
-            )}
-
-            <div className="w-1/2 px-1 py-1">
-              <AdminNavbarGrid
-                numCols={2}
-                sectionTitle="Temporary Scans"
-                options={scanList
-                  .filter((scan) => !scan.isPermanentScan)
-                  .map((scan) => ({
-                    optionName: scan.name,
-                    onClick: () => setCurrentScan(scan),
-                  }))}
-              />
-            </div>
-
-            <div className="px-1 py-1">
-              <AdminNavbarColumn
-                sectionTitle="Permanent Scans"
-                options={scanList
-                  .filter((scan) => scan.isPermanentScan)
-                  .map((scan) => ({
-                    optionName: scan.name,
-                    onClick: () => setCurrentScan(scan),
-                  }))}
-              />
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>,
-    );
     itemIdx++;
 
     return items;
@@ -238,19 +232,19 @@ export default function AppHeader2_Core(props: Props) {
       {/* Real navbar */}
       <div
         id="nav-bar"
-        className="relative font-dmSans border-[3px] border-[rgba(30,30,30,0.60)] rounded-xl px-16 p-1 bg-white opacity-95 text-[#40B7BA] cursor-pointer w-[60%] lg:w-[50%]"
+        className="relative font-dmSans border-[3px] border-[rgba(30,30,30,0.60)] rounded-xl p-1 bg-white opacity-90 text-[#40B7BA] cursor-pointer w-[70%] lg:w-[60%] xl:w-[50%]"
       >
         {/* Sign out button */}
         <button
           className={clsx(
-            'w-[112px]',
+            'w-[102px]',
             'top-1/2 -translate-y-1/2',
             `absolute ${
               location.pathname === '/'
-                ? '-left-[calc(112px+2rem)] lg:left-[unset] lg:-right-[calc(112px+2rem)]'
-                : '-right-[calc(112px+2rem)] lg:-right-[calc(112px+2rem)]'
+                ? '-left-[calc(102px+0.5rem)] lg:left-[unset] lg:-right-[calc(102px+1rem)] xl:-right-[calc(102px+2rem)]'
+                : '-right-[calc(102px+0.5rem)] lg:-right-[calc(102px+2rem)]'
             }`,
-            'py-3 px-5 rounded-[30px] bg-[#40B7BA] font-bold text-white ml-3 border-2 border-white',
+            'text-sm py-3 px-4 rounded-[30px] bg-[#40B7BA] font-bold text-white border-2 border-white',
           )}
           onClick={async () => {
             if (user) {
@@ -265,15 +259,12 @@ export default function AppHeader2_Core(props: Props) {
 
         <FloatingDockWrapper
           classes={{
-            wrapperDiv: clsx('gap-6 flex items-center justify-center'),
+            wrapperDiv: clsx('gap-4 flex items-center justify-center'),
           }}
           items={mainDockItems()}
         />
 
         <QRScanDialog scan={currentScan} onModalClose={() => setCurrentScan(null)} />
-
-        {/* Admin menu */}
-        {isAdmin && <FloatingDockWrapper items={adminDockItems()} />}
       </div>
     </div>
   );
