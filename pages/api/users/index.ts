6bc0e3c69e5c5266a9d6db9dadbf24ffc288df2d
DedsecKnight <mikenguyen7268@gmail.com>
2024-11-06 14:49:28 -0600

    feat: Shuffled apps

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -28,6 +28,19 @@ export interface UserData {
   };
 }
 
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+}
 /**
  *
  * API endpoint to fetch all users from the database
@@ -377,6 +390,7 @@ async function getAllRegistrations(req: NextApiRequest, res: NextApiResponse) {
     ...commonAppWithScores,
   ];
 
+  shuffle(data);
   // Hide sensitive data
   const hideSensitiveData = (data: Registration[]) => {
     return data.map((d) => ({
