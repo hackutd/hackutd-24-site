c7fb004202f9537cef99c3b33c18101c4d43b3f3
Rayyan Waris <raywa04@gmail.com>
2024-11-12 00:57:52 -0600

    feat: Fix image width

--- components/homeComponents/HomeSpeakers2.tsx
+++ components/homeComponents/HomeSpeakers2.tsx
@@ -5,11 +5,17 @@ import Image from 'next/image';
 export default function HomeSpeakers() {
   return (
     <section className="overflow-x-auto min-h-[24rem] pb-20">
-      <h1 className="text-5xl font-bold mb-3 text-center relative font-jua z-10  text-[#F7CE79] text-stroke">
+      <h1 className="text-5xl font-bold mb-3 text-center relative font-jua z-10 text-[#F7CE79] text-stroke">
         Keynote Speaker
       </h1>
       <div className="flex flex-col md:flex-row items-center md:items-stretch gap-3 mx-auto justify-center">
-        <Image className="w-fit" src={SpeakerImage} width={300} height={300} alt="Speaker" />
+        <Image
+          className="md:w-[400px] md:h-[400px]"
+          src={SpeakerImage}
+          width={200}
+          height={200}
+          alt="Speaker"
+        />
         <div className="w-4/5 bg-white rounded-3xl md:w-1/2 px-8 py-10 flex flex-col gap-y-2">
           <h1 className="font-fredoka font-medium text-[#F7CE79] text-4xl">Gen Fukunaga</h1>
           <h3 className="font-fredoka text-xl">CEO of Funimation</h3>
